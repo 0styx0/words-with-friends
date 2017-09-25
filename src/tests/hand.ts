50e80f457721e81cbe50a00e4039a990ed850024
DovidM <landyx47@gmail.com>
2017-09-25 16:03:11 -0400

    Made some test skeletons

---
+++ src/tests/hand.ts
@@ -0,0 +1,25 @@
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('hand', () => {
+
+    it('starts with 7 letters', () => {
+
+    });
+
+    it('loses a tile when placed on the board', () => {
+
+
+    });
+
+    it('regains tile when retrieved, unused from board', () => {
+
+
+    });
+
+    it(`at the end of a turn is refilled from tilebag`, () => {
+
+
+    });
+});
---
+++ src/tests/tiles.ts
@@ -0,0 +1,16 @@
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('tiles', () => {
+
+    it('have points', () => {
+
+
+    });
+
+    it('goes up in point value when on a multiplier', () => {
+
+
+    });
+});
