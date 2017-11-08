fcf1c12e8d869553b5b7e6f29e2f88875671297d
DovidM <landyx47@gmail.com>
2017-11-08 14:38:44 -0500

    Added function, check if tiles are placed validly when vertical

--- words-with-friends/src/classes/Game.tsx
+++ words-with-friends/src/classes/Game.tsx
@@ -15,7 +15,7 @@ export default class Game extends React.Component<{}, State> {
     static board = new Map<string, {
         filled: boolean,
         turnTileWasPlaced: number,
-        canPlace: boolean
+        recent: boolean
     }>();
 
     constructor() {
@@ -64,9 +64,52 @@ export default class Game extends React.Component<{}, State> {
 
         this.setHands();
 
+        const recentlyPlacedCoordinates: [number, number][] = [];
+
         Game.board.forEach((value, key) => {
+
             Game.board.set(key, value);
+
+            if (value.recent) {
+
+                const coordinates = key.split(', ');
+
+                recentlyPlacedCoordinates.push([+coordinates[0], +coordinates[1]]);
+            }
         });
+
+        this.checkTilePlacementValidity(recentlyPlacedCoordinates);
+    }
+
+
+    /**
+     * Checks if tiles are placed in a valid manner (straight horizontal or vertical)
+     *
+     * @param coordinates - 2d array of coordinates of tiles. Must be left to right
+     */
+    checkTilePlacementValidity(coordinates: [number, number][]) {
+
+        const validateVertically = (currentCoordinate: [number, number], indexOfCurrentCoordinate: number) => {
+
+            const belowPreviousCoordinate = currentCoordinate[0] === coordinates[indexOfCurrentCoordinate - 1][0] + 1;
+            const coordinateHasSameX = currentCoordinate[1] === coordinates[indexOfCurrentCoordinate - 1][1];
+
+            return belowPreviousCoordinate && coordinateHasSameX;
+        };
+
+        const tilesArePlacedVertically = validateVertically(coordinates[1], 1);
+
+        if (tilesArePlacedVertically) {
+
+            for (let i = 1; i < coordinates.length; i++) {
+
+                if (!validateVertically(coordinates[i], i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     render() {
