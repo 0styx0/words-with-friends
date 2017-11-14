66fc1c7c851e95985d30f2cfc7680bba70c198c5
DovidM <landyx47@gmail.com>
2017-11-14 14:16:18 -0500

    Made into class

--- words-with-friends/src/interfaces/Powerup.ts
+++ words-with-friends/src/interfaces/Powerup.ts
@@ -1,6 +1,29 @@
-export default interface Powerup {
+export default class Powerup {
+
     target: 'letter' | 'word';
     multiplyBy: number;
+    name: 'DL' | 'DW' | 'TL' | 'TW' | '';
+
+    constructor(target: 'letter' | 'word', multiplyBy: number) {
+
+        this.target = target;
+        this.multiplyBy = multiplyBy;
+
+        this.name = this.setPowerup();
+
+    }
+
+    setPowerup() {
+
+        switch (this.target) {
+            case 'letter':
+                return this.multiplyBy === 2 ? 'DL' : 'TL';
+            case 'word':
+                return this.multiplyBy === 2 ? 'DW' : 'TW';
+            default:
+                return '';
+        }
+    }
 }
 
 
