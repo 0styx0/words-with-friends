fdbfd162dc53240a289569b83e693b86400fa031
DovidM <landyx47@gmail.com>
2017-11-14 14:16:55 -0500

    Added powerup name to tileholders

--- words-with-friends/src/components/TileHolder/index.tsx
+++ words-with-friends/src/components/TileHolder/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './index.css';
 import TileType from '../../interfaces/Tile';
 import TileContainer from '../Tile/Tile';
+import Game from '../../classes/Game';
 
 interface Props {
     tile?: TileType;
@@ -12,6 +13,12 @@ interface Props {
 
 export default function TileHolder(props: Props) {
 
+    let powerupName = '';
+
+    if (Game.board.get(props.coordinates) && Game.board.get(props.coordinates)!.powerup) {
+        powerupName = Game.board.get(props.coordinates)!.powerup!.name;
+    }
+
     return (
         props.tile ? (
             <TileContainer
@@ -21,6 +28,6 @@ export default function TileHolder(props: Props) {
               canDrag={props.canDrag}
               coordinates={props.coordinates}
             />
-        ) : <span />
+        ) : <span>{powerupName}</span>
     );
 }
