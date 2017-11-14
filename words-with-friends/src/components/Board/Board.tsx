3d7e527cd824723538c96c63ec87120b0dd84f65
DovidM <landyx47@gmail.com>
2017-11-14 14:17:20 -0500

    Random powerups added to tileholders

--- words-with-friends/src/components/Board/Board.tsx
+++ words-with-friends/src/components/Board/Board.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Board from './';
 import TileHolder from '../TileHolder/TileHolder';
 import Game from '../../classes/Game';
+import TileInfo from '../../interfaces/TileInfo';
+import Powerup from '../../interfaces/Powerup';
 
 interface State {
     board: typeof TileHolder[];
@@ -27,21 +29,41 @@ export default class BoardContainer extends React.Component<{}, State> {
 
             for (let j = 0; j < 15; j++) {
 
-                board[i][j] = <TileHolder coordinates={`${i}, ${j}`} canDrag={true} key={`${i}, ${j}`} />;
-
-                // const centerTile = i === 7 && j === 7;
-
-                Game.board.set(`${i}, ${j}`, {
+                const tileInfo: TileInfo = {
                     filled: false,
                     turnTileWasPlaced: 0,
                     recent: false
-                });
+                };
+
+                if (Math.random().toString()[2] === '2') {
+
+                    tileInfo.powerup = this.setPowerup();
+                }
+
+                Game.board.set(`${i}, ${j}`, tileInfo);
+
+                board[i][j] = (
+                    <TileHolder
+                      coordinates={`${i}, ${j}`}
+                      canDrag={true}
+                      key={`${i}, ${j}`}
+                      powerup={tileInfo.powerup}
+                    />
+                );
             }
         }
 
         this.setState({
             board
         });
+    }
+
+    /**
+     * Randomly sets powerups
+     */
+    setPowerup(): Powerup | undefined {
+
+        return new Powerup(Math.random() > 0.5 ? 'letter' : 'word', Math.random() > 0.5 ? 2 : 3);
     }
 
     render() {
