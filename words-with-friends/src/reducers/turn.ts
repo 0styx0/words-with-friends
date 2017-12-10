b9a203292db6e2ef04982b6e3236102712628975
DovidM <landyx47@gmail.com>
2017-12-10 16:50:00 -0500

    On game initialization, turn = 1

--- words-with-friends/src/reducers/turn.ts
+++ words-with-friends/src/reducers/turn.ts
@@ -1,13 +1,14 @@
 import types from '../actions/types';
-import { Turn } from '../actions/interfaces';
+import { Turn, Board } from '../actions/interfaces';
 
-export default function turn(currentTurn: number = 1, action: Turn) {
+export default function turn(currentTurn: number = 1, action: Turn | Board) {
 
     switch (action.type) {
 
         case types.INCREMENT_TURN:
             return action.turn + 1;
-
+        case types.INIT_BOARD:
+            return 1;
         default:
             return currentTurn;
     }
