ff898ea704a30f4b35dc71bde336fa2cc6d1baa4
DovidM <landyx47@gmail.com>
2017-12-13 14:07:18 -0500

    Consolidated Board into 1 function

--- words-with-friends/src/components/Board/Board.tsx
+++ words-with-friends/src/components/Board/Board.tsx
@@ -1,45 +1,31 @@
 import * as React from 'react';
-import Board from './';
+import './index.css';
+
 import BoardTileHolder from '../TileHolder/TileHolder';
 
-interface State {
-    board: typeof BoardTileHolder[];
-}
+export default function Board() {
 
-export default class BoardContainer extends React.Component<{}, State> {
+    let board: typeof BoardTileHolder[] = [];
 
-    constructor() {
-        super();
+    const boardDimensions = 15;
 
-        this.state = {
-            board: []
-        };
-    }
+    for (let i = 0; i < boardDimensions; i++) {
 
-    componentDidMount() {
+        for (let j = 0; j < boardDimensions; j++) {
 
-        let board: typeof BoardTileHolder[] = [];
-
-        for (let i = 0; i < 15; i++) {
-
-            for (let j = 0; j < 15; j++) {
-
-                board.push(
-                    <BoardTileHolder
-                        coordinates={`${i}, ${j}`}
-                        key={`${i}, ${j}`}
-                        {...[] as any}
-                    /> as {} as typeof BoardTileHolder
-                );
-            }
+            board.push(
+                <BoardTileHolder
+                    coordinates={`${i}, ${j}`}
+                    key={`${i}, ${j}`}
+                    {...[] as any}
+                /> as {} as typeof BoardTileHolder
+            );
         }
-
-        this.setState({
-            board
-        });
     }
 
-    render() {
-        return <Board board={this.state.board} />;
-    }
+    return (
+        <div className="wrapper">
+            {board}
+        </div>
+    );
 }
