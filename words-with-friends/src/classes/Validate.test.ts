677ebd84bbaf616a6bd05863334a5762eedc2dbf
DovidM <landyx47@gmail.com>
2017-12-13 14:33:45 -0500

    Started test skeleton for travelHorizontally

---
+++ words-with-friends/src/classes/Validate.test.ts
@@ -0,0 +1,26 @@
+
+describe('Validate', () => {
+
+    describe('#travelHorizontally', () => {
+
+        it('stops if callback returns `false`', () => {
+
+        });
+
+        it('only travels in the line specified (nowhere else)', () => {
+
+        });
+
+        it('calls `callback` param on every space it comes across', () => {
+
+        });
+
+        it('can go backwards', () => {
+
+        });
+
+        it(`doesn't go past the end of the board`, () => {
+
+        });
+    });
+});
