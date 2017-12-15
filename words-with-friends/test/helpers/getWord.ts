773d08846d95cfc3723c67053ce645a532919ced
DovidM <landyx47@gmail.com>
2017-12-15 11:37:22 -0500

    Helper to get a word by length

--- words-with-friends/test/helpers/getWord.ts
+++ words-with-friends/test/helpers/getWord.ts
@@ -1,12 +1,19 @@
-const dictionary = require('dictionary');
+import * as casual from 'casual';
+import * as wordList from 'word-list-json';
 
 /**
  *
- * @param length - of word to get
+ * @param length - of word to get. If 0, get random word
  *
  * @return valid words-with-friends word with length `length`
  */
-export default function getWord(length: number) {
+export default function getWord(length: number = 0): string {
 
-    return (dictionary.words as string[])[dictionary.lengths[length]];
+    const indexOfWordWithLength = wordList.lengths[length];
+
+    const word: string = (length === 0) ?
+        casual.random_element(wordList as {} as string[]) :
+        wordList[indexOfWordWithLength];
+
+    return word.toUpperCase();
 }
