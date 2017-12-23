e0b87349e9ff78675238d38137042b02f44648c3
DovidM <landyx47@gmail.com>
2017-12-23 16:17:12 -0500

    Added test skeleton

---
+++ src/classes/Tilebag.test.ts
@@ -0,0 +1,31 @@
+import TileInfo from './TileInfo';
+import * as casual from 'casual';
+
+describe('Tilebag', () => {
+
+    describe('constructor', () => {
+
+        it('filled up .tiles with `alphabet`, adding each letter `alphabet.$letter.amount` times', () => {
+
+
+        });
+    });
+
+    describe('#getRandomTile', () => {
+
+        it('returns a copy of tile', () => {
+
+
+        });
+
+        it('removes tile returned from .tiles', () => {
+
+
+        });
+
+        it(`sets tile.playerIndex to index passed`, () => {
+
+
+        });
+    });
+});
