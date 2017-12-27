1117dd7d0f0bb4e9b5fa214673c4b3379b7b1e49
DovidM <landyx47@gmail.com>
2017-12-27 13:55:15 -0500

    Added enzyme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,7 @@
 import * as dotEnv from 'dotenv-safe';
 dotEnv.load({ path: '../' });
+
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
