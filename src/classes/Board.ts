72bd41ec426143aee78e2bbc688f74f04713914a
DovidM <landyx47@gmail.com>
2017-12-28 21:46:05 -0500

    Added Readonly modifiers

--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -2,7 +2,7 @@ import TileInfo from './TileInfo';
 
 export default class Board extends Map {
 
-    constructor(board?: Board) {
+    constructor(board?: Readonly<Board>) {
         super();
 
         // copies board
@@ -11,11 +11,11 @@ export default class Board extends Map {
         }
     }
 
-    set(coordinates: number[], value: TileInfo) {
+    set(coordinates: ReadonlyArray<number>, value: Readonly<TileInfo>) {
         return super.set(`${coordinates[0]}, ${coordinates[1]}`, value);
     }
 
-    get(coordinates: number[]): TileInfo | undefined {
+    get(coordinates: ReadonlyArray<number>): Readonly<TileInfo> | undefined {
         return coordinates ? super.get(`${coordinates[0]}, ${coordinates[1]}`) : undefined;
     }
 }
