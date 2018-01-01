6821ea2aa030a6983b71c978621f37d72785078e
DovidM <landyx47@gmail.com>
2018-01-01 10:20:28 -0500

    Added tests

--- src/reducers/board.test.ts
+++ src/reducers/board.test.ts
@@ -1,7 +1,11 @@
 import board from './board';
 import BoardClass from '../classes/Board';
 import types from '../actions/types';
-
+import { getState } from '../store';
+import * as casual from 'casual';
+import Tile from '../interfaces/Tile';
+import Player from '../classes/Player';
+import TileInfo from '../classes/TileInfo';
 
 describe('board', () => {
 
@@ -16,7 +20,6 @@ describe('board', () => {
 
             expect(newBoard).toBeInstanceOf(BoardClass);
             expect(newBoard.size).toBe((+process.env.REACT_APP_BOARD_DIMENSIONS!) ** 2);
-
         });
     });
 
@@ -24,7 +27,18 @@ describe('board', () => {
 
         it('puts correct tile on board at proper coordinates', () => {
 
+            const tile = getState().Tilebag.getRandomTile(0);
+            const coordinates = casual.array_of_integers(2);
 
+            const boardWithTile = board(new BoardClass(), {
+                type: types.PLACE_TILE_ON_BOARD,
+                tile,
+                currentPlayer: casual.random_element(getState().Players),
+                currentTurn: casual.integer(),
+                coordinates
+            });
+
+            expect(boardWithTile.get(coordinates)!.tile).toEqual(tile);
         });
     });
 
@@ -32,7 +46,19 @@ describe('board', () => {
 
         it('removes tile as coordinates given', () => {
 
+            const coordinates = casual.array_of_integers(2);
+            const initialBoard = new BoardClass();
+            const tile = getState().Tilebag.getRandomTile(0);
+            const tileInfo = new TileInfo();
+            tileInfo.place(tile, casual.random_element(getState().Players), getState().turn);
+            initialBoard.set(coordinates, tileInfo);
 
+            const boardWithoutTile = board(initialBoard, {
+                type: types.REMOVE_TILE_FROM_BOARD,
+                coordinates
+            });
+
+            expect(boardWithoutTile.get(coordinates)).toEqual(new TileInfo());
         });
     });
 });
