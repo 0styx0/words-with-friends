f0b68ae790151b577c898f8e593bd246885c96f6
DovidM <landyx47@gmail.com>
2018-01-11 07:37:26 -0500

    Added clone method

--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -2,13 +2,8 @@ import TileInfo from './TileInfo';
 
 export default class Board extends Map {
 
-    constructor(board?: Readonly<Board>) {
+    constructor() {
         super();
-
-        // copies board
-        if (board) {
-            board.forEach((value, key) => super.set(key, value));
-        }
     }
 
     set(coordinates: ReadonlyArray<number>, value: Readonly<TileInfo>) {
@@ -17,5 +12,27 @@ export default class Board extends Map {
 
     get(coordinates: ReadonlyArray<number>): Readonly<TileInfo> | undefined {
         return coordinates ? super.get(`${coordinates[0]}, ${coordinates[1]}`) : undefined;
+    }
+
+    clone() {
+
+        const copy = new Board();
+
+        this.forEach((value: Readonly<TileInfo>, key: string) =>
+            copy.set(this.convertIntStringIntoArray(key), value));
+
+        return copy;
+    }
+
+    /**
+     * @param coordinate - 'n, n' where n is a digit
+     *
+     * @return [n, n] where n is coerced to number
+     */
+    private convertIntStringIntoArray(coordinate: string) {
+
+        const keys = coordinate.split(/[^\d]\s/);
+
+        return keys.map(key => +key);
     }
 }
